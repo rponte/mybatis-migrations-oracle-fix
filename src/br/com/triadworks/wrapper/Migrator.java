140b97b3621db1dc2b3bdbdaef9c1e60181830cd
Rafael Ponte <rponte@gmail.com>
2013-06-28 15:05:40 -0300

    Adds just a simple comment :-)

--- src/br/com/triadworks/wrapper/Migrator.java
+++ src/br/com/triadworks/wrapper/Migrator.java
@@ -3,7 +3,7 @@ package br.com.triadworks.wrapper;
 public class Migrator {
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("line.separator", "\n");
+		System.setProperty("line.separator", "\n"); // the magic is here <3
 		org.apache.ibatis.migration.Migrator.main(args);
 	}
 	
